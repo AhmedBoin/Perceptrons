f75da0b8d43e1f86b37d7cf663a1e8be07273d57
Ahmed Boin <90623804+AhmedBoin@users.noreply.github.com>
2023-01-05 03:15:45 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,9 @@ fn main() {
         let db = b.grad().unwrap() * lr;
         w.optimize_with_dyn_array(dw);
         b.optimize_with_dyn_array(db);
+
+        w.zero_grad();
+        b.zero_grad();
     }
     println!("w = {w}\nb = {b}");
 }
